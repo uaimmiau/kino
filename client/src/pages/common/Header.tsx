fc4101a9cd087dc60cd5f79e921b47dd2e8f83d4
uaimmiau <zoltanchivay2137@gmail.com>
2025-01-28 19:29:06 +0100

    Starting to add some of them styles

--- client/src/pages/common/Header.tsx
+++ client/src/pages/common/Header.tsx
@@ -6,22 +6,27 @@ export default function Header() {
     const { user, logout } = AuthData();
     return (
         <div className="header">
-            <img src="/react.svg" alt="logo" />
-
-            <Link to={`/`}>Strona główna</Link>
-            {user.isAuthenticated ? (
-                <Link to={`/`} onClick={logout}>
-                    Wyloguj
-                </Link>
-            ) : (
-                <Link to={`/login`}>Zaloguj</Link>
-            )}
-            {user.isAuthenticated ? (
-                false
-            ) : (
-                <Link to={`/register`}>Zarejestruj</Link>
-            )}
-            {user.isAuthenticated ? <Link to={`/account`}>Konto</Link> : false}
+            <img id="logo" src="/logo.png" alt="logo" />
+            <div className="headerCont">
+                <Link to={`/`}>Strona główna</Link>
+                {user.isAuthenticated ? (
+                    <Link to={`/`} onClick={logout}>
+                        Wyloguj
+                    </Link>
+                ) : (
+                    <Link to={`/login`}>Zaloguj</Link>
+                )}
+                {user.isAuthenticated ? (
+                    false
+                ) : (
+                    <Link to={`/register`}>Zarejestruj</Link>
+                )}
+                {user.isAuthenticated ? (
+                    <Link to={`/account`}>Konto</Link>
+                ) : (
+                    false
+                )}
+            </div>
         </div>
     );
 }
